5ac83b3af3e63620ab8963b7fdf969130a3e5c72
Edouard de Laage <edouarddelaage@gmail.com>
2019-01-16 10:54:18 +0100

    fix bug eslint

--- src/components/AsidePres.js
+++ src/components/AsidePres.js
@@ -20,8 +20,8 @@ const AsidePres = () => {
           <p>Javascript / Node JS / React</p>
         </div>
         <div className="social-networks">
-          <a className="fa fa-linkedin link-aside" href="https://www.linkedin.com/in/edouard-de-laage-4711818/" target="_blank" rel="noopener noreferrer"></a>
-          <a className="fa fa-github link-aside" href="https://github.com/eddelaage" target="_blank" rel="noopener noreferrer"></a>
+          <a href="https://www.linkedin.com/in/edouard-de-laage-4711818/" target="_blank" rel="noopener noreferrer"><i className="fa fa-linkedin link-aside"></i></a>
+          <a href="https://github.com/eddelaage" target="_blank" rel="noopener noreferrer"><i className="fa fa-github link-aside"></i></a>
         </div>
         <div className="contact">
           <p className="link-aside">06 07 73 62 26</p>
